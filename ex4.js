94695bc388a2d7872fb003a196cdd890e52ad375
ClementG60 <cle_guillaume@hotmail.fr>
2021-11-05 10:45:10 +0100

    ajout fichier et code html+js

---
+++ ex4.js
@@ -0,0 +1,15 @@
+function imgChange(x){
+    x.src = "Joconde2.jpg"
+}
+
+function imgChange2(x){
+    x.src = "Joconde.jpg"
+}
+
+function imgScale(x){
+    x.style.height = "600px"
+}
+
+function imgScale2(x){
+    x.style.height = "300px"
+}
